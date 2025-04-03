39ce6ee84bff0963cf2af423b248129b42ed06cb
PalmDevs <git@palmdevs.me>
2025-04-03 22:59:01 +0700

    feat(lib/utils/types): add `LogicalOr` type

--- lib/utils/src/types.d.ts
+++ lib/utils/src/types.d.ts
@@ -1,3 +1,4 @@
 export type Nullish = null | undefined
 export type If<T, Then, Else> = T extends true ? Then : Else
 export type AnyObject = Record<any, any>
+export type LogicalOr<T1, T2> = T1 extends true ? true : T2 extends true ? true : false
