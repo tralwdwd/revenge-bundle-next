8fcbcb5792a75fdd22461634484ad75b152545a0
PalmDevs <git@palmdevs.me>
2025-04-15 15:04:35 +0700

    feat(lib/modules/metro/subscriptions): clean up specific subscriptions once called

--- lib/modules/src/metro/subscriptions/_internal.ts
+++ lib/modules/src/metro/subscriptions/_internal.ts
@@ -12,13 +12,19 @@ export const _importedPath = new Set<ModuleWithImportedPathInitializedCallback>(
 export function _executeRequiredSubscription() {
     const id = _mInitingId
     for (const cb of _reqAll) cb(id)
-    if (_reqSpecific.has(id)) for (const cb of _reqSpecific.get(id)!) cb(id)
+    if (_reqSpecific.has(id)) {
+        for (const cb of _reqSpecific.get(id)!) cb(id)
+        _reqSpecific.delete(id)
+    }
 }
 
 export function _executeInitializedSubscription(exports: Metro.ModuleExports) {
     const id = _mInitingId
     for (const cb of _initAll) cb(id, exports)
-    if (_initSpecific.has(id)) for (const cb of _initSpecific.get(id)!) cb(id, exports)
+    if (_initSpecific.has(id)) {
+        for (const cb of _initSpecific.get(id)!) cb(id, exports)
+        _initSpecific.delete(id)
+    }
 }
 
 export function _executeImportedPathSubscription(path: string) {
