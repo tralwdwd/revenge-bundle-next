fd1f41f3b968b7feaf02156b7d39d2f978089719
PalmDevs <git@palmdevs.me>
2025-04-27 20:56:51 +0700

    fix(lib/modules/common)!: make exports capitalization consistent

--- lib/modules/src/common.ts
+++ lib/modules/src/common.ts
@@ -5,8 +5,8 @@ import { byProps } from './finders/filters'
 
 export const ReactModuleId = yielded(lookupModuleIds(byProps<typeof import('react')>('useState')), 1)!
 export const ReactNativeModuleId = lookupModuleId(byProps<typeof import('react-native')>('AppRegistry'))!
-export const ReactJSXRuntimeModuleId = yielded(lookupModuleIds(byProps<typeof import('react/jsx-runtime')>('jsx'))!, 1)
+export const ReactJsxRuntimeModuleId = yielded(lookupModuleIds(byProps<typeof import('react/jsx-runtime')>('jsx'))!, 1)
 
 export const React = __r(ReactModuleId) as typeof import('react')
 export const ReactNative = __r(ReactNativeModuleId) as typeof import('react-native')
-export const ReactJSXRuntime = __r(ReactJSXRuntimeModuleId) as typeof import('react/jsx-runtime')
+export const ReactJsxRuntime = __r(ReactJsxRuntimeModuleId) as typeof import('react/jsx-runtime')
