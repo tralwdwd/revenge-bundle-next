c61b6136fff875ba5127fa3e1e4e49db3535e53a
PalmDevs <git@palmdevs.me>
2025-04-27 14:46:14 +0700

    fix(lib/init): initialize all revenge libraries

--- lib/init/src/index.ts
+++ lib/init/src/index.ts
@@ -3,28 +3,34 @@ import './preinit'
 import './patches/prevent-immutablize'
 import './patches/proxy'
 
-import { onModuleFirstRequired } from '@revenge-mod/modules/metro/subscriptions'
+import { onModuleFirstRequired, onModuleInitialized } from '@revenge-mod/modules/metro/subscriptions'
 import { getErrorStack } from '@revenge-mod/utils/errors'
 
 import { logger } from './shared'
 
-onModuleFirstRequired(0, async function onceIndexModuleRequired() {
+onModuleFirstRequired(0, function onceIndexModuleRequired() {
     if (!__BUILD_FLAG_INIT_DISABLE_PATCH_LOG_PROMISE_REJECTIONS__) require('./patches/log-promise-rejections')
 
-    try {
-        logger.log('Revenge loaded!')
-    } catch (e) {
-        const { ClientInfoModule, DeviceModule } = require('@revenge-mod/modules/native') as
-            // biome-ignore format: Don't format this please
-            typeof import('@revenge-mod/modules/native')
+    require('@revenge-mod/modules')
 
-        // TODO(init): Move to use native module
-        alert(
-            // biome-ignore lint/style/useTemplate: I want this to be readable, thank you
-            'Failed to load Revenge!\n' +
-                `Discord: ${ClientInfoModule.Version} (${ClientInfoModule.Build})\n` +
-                `Device: ${DeviceModule.deviceManufacturer} ${DeviceModule.deviceModel}\n\n` +
-                getErrorStack(e),
-        )
-    }
+    onModuleInitialized(0, () => {
+        require('@revenge-mod/assets')
+
+        try {
+            logger.log('Revenge loaded!')
+        } catch (e) {
+            const { ClientInfoModule, DeviceModule } = require('@revenge-mod/modules/native/discord') as
+                // biome-ignore format: Don't format this please
+                typeof import('@revenge-mod/modules/native/discord')
+
+            // TODO(init): Move to use native module
+            alert(
+                // biome-ignore lint/style/useTemplate: I want this to be readable, thank you
+                'Failed to load Revenge!\n' +
+                    `Discord: ${ClientInfoModule.Version} (${ClientInfoModule.Build})\n` +
+                    `Device: ${DeviceModule.deviceManufacturer} ${DeviceModule.deviceModel}\n\n` +
+                    getErrorStack(e),
+            )
+        }
+    })
 })
