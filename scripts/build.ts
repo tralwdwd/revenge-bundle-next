dbcbcdb14b5610c7026736ce76d6cfa7963da823
PalmDevs <git@palmdevs.me>
2025-04-30 18:35:50 +0700

    fix(scripts/build): edit transform rules

--- scripts/build.ts
+++ scripts/build.ts
@@ -82,20 +82,25 @@ function swcPlugin() {
                         // https://github.com/discord/hermes/blob/main/doc/Features.md
                         targets: 'fully supports es6',
                         include: [
+                            'transform-async-generator-functions',
                             'transform-block-scoping',
                             'transform-classes',
-                            'transform-async-to-generator',
-                            'transform-async-generator-functions',
+                            'transform-duplicate-named-capturing-groups-regex',
                             'transform-named-capturing-groups-regex',
                         ],
                         exclude: [
+                            // Async functions are supported, only async arrow functions aren't
+                            // Source: https://github.com/facebook/hermes/issues/1395
+                            'transform-async-to-generator',
+                            'transform-exponentiation-operator',
+                            'transform-logical-assignment-operators',
+                            'transform-nullish-coalescing-operator',
+                            'transform-numeric-separator',
+                            'transform-object-rest-spread',
+                            'transform-optional-catch-binding',
+                            'transform-optional-chaining',
                             'transform-parameters',
                             'transform-template-literals',
-                            'transform-exponentiation-operator',
-                            'transform-nullish-coalescing-operator',
-                            'transform-object-rest-spread',
-                            'transform-optional-chaining',
-                            'transform-logical-assignment-operators',
                         ],
                     },
                 })
