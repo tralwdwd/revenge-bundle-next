9c6ad05e2b8d3e0fc17669eefaac521003e6874d
PalmDevs <git@palmdevs.me>
2025-05-05 19:29:50 +0700

    feat(plugins/settings/components/AssetIcon): safely access nullable asset

--- src/plugins/settings/components/icons/AssetIcon.tsx
+++ src/plugins/settings/components/icons/AssetIcon.tsx
@@ -3,7 +3,7 @@ import type { DiscordModules } from '@revenge-mod/discord/types'
 import { Components } from '@revenge-mod/discord/ui'
 
 export default function AssetIcon(props: AssetIconProps) {
-    return <Components.TableRow.Icon source={getAssetByName(props.name)!.id} {...props} />
+    return <Components.TableRow.Icon source={getAssetByName(props.name)?.id!} {...props} />
 }
 
 export interface AssetIconProps extends Omit<DiscordModules.Components.TableRowIconProps, 'source'> {
