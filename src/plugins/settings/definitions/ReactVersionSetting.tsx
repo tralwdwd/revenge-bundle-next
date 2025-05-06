4371b95bd93ff03adf40483ecbdffca703443c39
PalmDevs <git@palmdevs.me>
2025-05-06 23:51:21 +0700

    fix(plugins/settings/definitions/ReactVersionSetting): switch to use `AssetIcon`

--- src/plugins/settings/definitions/ReactVersionSetting.tsx
+++ src/plugins/settings/definitions/ReactVersionSetting.tsx
@@ -1,14 +1,14 @@
-import { getAssetByName } from '@revenge-mod/assets'
-import { Components } from '@revenge-mod/discord/ui'
 import { React } from '@revenge-mod/react'
 
 import { MobileSetting } from '../constants'
+
+import AssetIcon from '~/components/icons/AssetIcon'
 
 import type { SettingsItem } from '@revenge-mod/discord/ui/settings'
 
 const ReactVersionSetting: SettingsItem = {
     parent: MobileSetting.REVENGE,
-    IconComponent: () => <Components.TableRow.Icon source={getAssetByName('ScienceIcon')!.id} />,
+    IconComponent: () => <AssetIcon name="ScienceIcon" />,
     title: () => 'React',
     useDescription: () => React.version,
     type: 'static',
