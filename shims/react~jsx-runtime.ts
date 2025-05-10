3818ee1313da07fe3b1a7cbc8f51c93ff632636a
PalmDevs <git@palmdevs.me>
2025-05-10 18:14:05 +0700

    fix(shims/react~jsx-runtime): correctly unproxify values

--- shims/react~jsx-runtime.ts
+++ shims/react~jsx-runtime.ts
@@ -1,13 +1,13 @@
 import { ReactJsxRuntime } from '@revenge-mod/react'
-import { getProxyTarget, isProxy } from '@revenge-mod/utils/proxy'
+import { isProxified, unproxify } from '@revenge-mod/utils/proxy'
 
 import type { ElementType } from 'react'
 
 const { jsx: _jsx, jsxs: _jsxs, Fragment } = ReactJsxRuntime
 
 const jsx = ((type, ...args) =>
-    _jsx(isProxy(type as object) ? (getProxyTarget(type as object) as ElementType) : type, ...args)) as typeof _jsx
+    _jsx(isProxified(type as object) ? (unproxify(type as object) as ElementType) : type, ...args)) as typeof _jsx
 const jsxs = ((type, ...args) =>
-    _jsxs(isProxy(type as object) ? (getProxyTarget(type as object) as ElementType) : type, ...args)) as typeof _jsxs
+    _jsxs(isProxified(type as object) ? (unproxify(type as object) as ElementType) : type, ...args)) as typeof _jsxs
 
 export { jsx, jsxs, Fragment }
