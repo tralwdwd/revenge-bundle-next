b4923296e5557754bfd432b3934a77b3ee823c5b
PalmDevs <git@palmdevs.me>
2025-06-12 23:23:16 +0700

    feat(lib/utils/errors): add `getCurrentStack`

--- lib/utils/src/errors.ts
+++ lib/utils/src/errors.ts
@@ -1,3 +1,8 @@
 export function getErrorStack(e: unknown) {
     return e instanceof Error ? e.stack : String(e)
 }
+
+export function getCurrentStack() {
+    const { stack } = new Error()
+    return stack!.split('\n').slice(2).join('\n')
+}
