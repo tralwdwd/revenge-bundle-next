9e6614a1bd9dc8d23fe0c2cad4a3517a56d73485
PalmDevs <git@palmdevs.me>
2025-07-10 21:26:19 +0700

    chore: lint and format

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,8 @@ import type { Metro } from '@revenge-mod/modules/types'
 Object.defineProperty(globalThis, '__c', {
     configurable: true,
     set(clear: Metro.ClearFn) {
+        // @ts-expect-error
+        // biome-ignore lint/performance/noDelete: Prevent infinite set loop
         delete globalThis.__c
         globalThis.__c = clear
 
