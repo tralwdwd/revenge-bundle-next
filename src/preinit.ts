8347e24e542c571baf62a6b6e9063d981d359442
PalmDevs <git@palmdevs.me>
2025-07-11 01:15:07 +0700

    chore(lib/react)!: remove `jsx-runtime`
    This has been made because no developer will likely ever require this API.
    
    The current API assumes you already have an instance of the component, which means you can likely already patch that component instance itself.
    
    The API can be made more powerful by taking any string names, and comparing it to the component's type name, type displayName, name, displayName, etc., but it will introduce performance bottlenecks.

--- src/preinit.ts
+++ src/preinit.ts
@@ -37,11 +37,10 @@ onModuleFirstRequired(IndexModuleId, function onIndexRequired() {
                     )
 
                 // Initialize init libraries
-                require('@revenge-mod/react/init')
                 require('@revenge-mod/storage/init')
-                require('@revenge-mod/externals/init')
                 require('@revenge-mod/discord/init')
                 require('@revenge-mod/components/init')
+                require('@revenge-mod/externals/init')
 
                 // Run all init plugins
                 require('~/plugins/init')
