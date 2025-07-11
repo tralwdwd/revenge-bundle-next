f4c5ea15a265b08f6919da11ee8c5ff6659c35a0
PalmDevs <git@palmdevs.me>
2025-07-11 15:19:57 +0700

    fix(plugins/logging): remove `__BUILD_ENV__` references

--- src/plugins/preinit/logging/index.ts
+++ src/plugins/preinit/logging/index.ts
@@ -1,6 +1,7 @@
 import { AppStartPerformance } from '@revenge-mod/discord/preinit'
 import { InternalPluginFlags, registerPlugin } from '@revenge-mod/plugins/_'
 import { PluginFlags } from '@revenge-mod/plugins/constants'
+import { BuildEnvironment } from '~/constants'
 
 AppStartPerformance.mark('👊', 'Plugins register')
 
@@ -32,7 +33,7 @@ registerPlugin(
                 performance.now() - tsInit,
             )
             logger.log(
-                `👊 Revenge. Discord, your way. (${__BUILD_VERSION__}-${__BUILD_COMMIT__}-${__BUILD_BRANCH__} (${__BUILD_ENV__}))`,
+                `👊 Revenge. Discord, your way. (${__BUILD_VERSION__}-${__BUILD_COMMIT__}-${__BUILD_BRANCH__} (${BuildEnvironment}))`,
             )
         },
     },
