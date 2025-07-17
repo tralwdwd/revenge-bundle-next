8ca50309eb0e04e0cd9e3917c3322d4485389750
PalmDevs <git@palmdevs.me>
2025-07-18 02:02:54 +0700

    chore(plugins/logging): only register on dev builds

--- src/plugins/preinit/logging/index.ts
+++ src/plugins/preinit/logging/index.ts
@@ -3,43 +3,52 @@ import { InternalPluginFlags, registerPlugin } from '@revenge-mod/plugins/_'
 import { PluginFlags } from '@revenge-mod/plugins/constants'
 import { BuildEnvironment, FullVersion } from '~/constants'
 
-AppStartPerformance.mark('👊', 'Plugins register')
+if (__DEV__) {
+    AppStartPerformance.mark('👊', 'Plugins register')
 
-const tsReg = performance.now()
-let tsPreInit: number
-let tsInit: number
+    const tsReg = performance.now()
+    let tsPreInit: number
+    let tsInit: number
 
-registerPlugin(
-    {
-        id: 'revenge.logging',
-        name: 'Logging',
-        description: "Extra logging for Revenge's internal modules",
-        author: 'Revenge',
-        icon: 'PaperIcon',
-    },
-    {
-        preInit() {
-            tsPreInit = performance.now()
-            AppStartPerformance.mark('👊', 'Plugins preInit', tsPreInit - tsReg)
+    registerPlugin(
+        {
+            id: 'revenge.logging',
+            name: 'Logging',
+            description: "Extra logging for Revenge's internal modules",
+            author: 'Revenge',
+            icon: 'PaperIcon',
         },
-        init() {
-            tsInit = performance.now()
-            AppStartPerformance.mark('👊', 'Plugins init', tsInit - tsPreInit)
-        },
-        start({ logger }) {
-            if (__DEV__)
+        {
+            preInit() {
+                tsPreInit = performance.now()
+                AppStartPerformance.mark(
+                    '👊',
+                    'Plugins preInit',
+                    tsPreInit - tsReg,
+                )
+            },
+            init() {
+                tsInit = performance.now()
+                AppStartPerformance.mark(
+                    '👊',
+                    'Plugins init',
+                    tsInit - tsPreInit,
+                )
+            },
+            start({ logger }) {
                 nativeLoggingHook(`\u001b[31m--- START STAGE ---\u001b[0m`, 1)
 
-            AppStartPerformance.mark(
-                '👊',
-                'Plugins start',
-                performance.now() - tsInit,
-            )
-            logger.log(
-                `👊 Revenge. Discord, your way. (${FullVersion} (${BuildEnvironment}))`,
-            )
+                AppStartPerformance.mark(
+                    '👊',
+                    'Plugins start',
+                    performance.now() - tsInit,
+                )
+                logger.log(
+                    `👊 Revenge. Discord, your way. (${FullVersion} (${BuildEnvironment}))`,
+                )
+            },
         },
-    },
-    PluginFlags.Enabled,
-    InternalPluginFlags.Internal,
-)
+        PluginFlags.Enabled,
+        InternalPluginFlags.Internal,
+    )
+}
