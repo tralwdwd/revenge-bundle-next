6852c816d0b895c553a071c23a4726eb525c9c36
PalmDevs <git@palmdevs.me>
2025-07-21 22:04:51 +0700

    chore(lib/plugins): add plugin start order log

--- lib/plugins/src/start.ts
+++ lib/plugins/src/start.ts
@@ -4,5 +4,11 @@ import { PluginFlags } from './constants'
 
 computePendingNodes()
 
+if (__DEV__)
+    nativeLoggingHook(
+        `\u001b[32mStarting plugins: ${pListOrdered.map(x => `\u001b[33m${x.manifest.id}\u001b[0m`).join(' -> ')}\u001b[0m`,
+        1,
+    )
+
 for (const plugin of pListOrdered)
     if (plugin.flags & PluginFlags.Enabled) startPlugin(plugin)
