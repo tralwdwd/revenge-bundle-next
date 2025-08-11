333ba5622e3f7c67f3ef807ebebd3f798916bbbb
PalmDevs <git@palmdevs.me>
2025-08-11 20:14:27 +0700

    fix(scripts/types): also generate definitions for index `components` import

--- scripts/types.ts
+++ scripts/types.ts
@@ -191,6 +191,7 @@ function library<
 const Libraries = [
     library(assets, ['', 'types']),
     library(components, [
+        '',
         'FormSwitch',
         'Page',
         'SearchInput',
