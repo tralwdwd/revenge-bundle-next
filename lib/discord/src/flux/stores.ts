ac3546a53de10f6942dafa396c636fe834028d8f
PalmDevs <git@palmdevs.me>
2025-08-13 00:50:36 +0700

    chore: make internal library imports more readable

--- lib/discord/src/flux/stores.ts
+++ lib/discord/src/flux/stores.ts
@@ -9,8 +9,8 @@ import {
 import { waitForModules } from '@revenge-mod/modules/finders/wait'
 import { getModuleDependencies } from '@revenge-mod/modules/metro/utils'
 import { asap, noop } from '@revenge-mod/utils/callback'
-import { cached, cacheFilterResultForId } from '../../../modules/src/caches'
-import { FilterResultFlags } from '../../../modules/src/finders/_internal'
+import { cached, cacheFilterResultForId } from '#modules/src/caches'
+import { FilterResultFlags } from '#modules/src/finders/_internal'
 import type {
     Filter,
     FilterGenerator,
