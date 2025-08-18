138b0dc65183ed54b305ec7c4a0cf2db22768267
PalmDevs <git@palmdevs.me>
2025-08-19 02:40:41 +0700

    chore(shims/react~jsx-runtime): revert to basic shimming

--- shims/react~jsx-runtime.ts
+++ shims/react~jsx-runtime.ts
@@ -1,15 +1,3 @@
 import { ReactJSXRuntime } from '@revenge-mod/react'
-import { isProxified, unproxify } from '@revenge-mod/utils/proxy'
-import type { ElementType } from 'react'
 
-const { jsx: _jsx, jsxs: _jsxs, Fragment } = ReactJSXRuntime
-
-const jsx: typeof _jsx = (type, props, key) => _jsx(unproxy(type), props, key)
-const jsxs: typeof _jsxs = (type, props, key) =>
-    _jsxs(unproxy(type), props, key)
-
-function unproxy(type: any): ElementType {
-    return isProxified(type) ? unproxify(type) : type
-}
-
-module.exports = { jsx, jsxs, Fragment }
+module.exports = ReactJSXRuntime
