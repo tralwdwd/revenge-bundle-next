094a6bea07021680ad837e3da45b1bf4c920f6c3
PalmDevs <git@palmdevs.me>
2025-08-19 22:14:45 +0700

    feat: use native bridge for alerting errors

--- src/init.ts
+++ src/init.ts
@@ -3,7 +3,7 @@ import '@revenge-mod/react/init'
 import '@revenge-mod/storage/init'
 
 import { onRunApplication } from '@revenge-mod/react/native'
-import { onError } from './preinit'
+import { onError } from '~preinit'
 
 const unsub = onRunApplication(() => {
     unsub()
